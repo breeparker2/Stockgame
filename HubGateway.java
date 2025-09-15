aa48f1958abdeb450708227243c52787366b1801
breeparker2 <115360558+breeparker2@users.noreply.github.com>
2025-09-15 17:23:53 +1000

    Add files via upload

---
+++ HubGateway.java
@@ -0,0 +1,17 @@
+import javafx.application.Platform;
+
+public final class HubGateway {
+    private static java.util.function.Consumer<MarketEvent> sink;
+
+    // Hub calls this once to register its EventBus
+    public static void register(java.util.function.Consumer<MarketEvent> s) {
+        sink = s;
+    }
+
+    // Anywhere else can call this to deliver an event to the hub
+    public static void publish(MarketEvent evt) {
+        if (sink != null) {
+            Platform.runLater(() -> sink.accept(evt));
+        }
+    }
+}
